a4f4382c0b179d391345ff9996095817a52e6682
links <3424854370@qq.com>
2024-08-29 21:01:03 -0700

    l

--- kernel/vm.c
+++ kernel/vm.c
@@ -112,36 +112,36 @@ walkcowaddr(pagetable_t pagetable, uint64 va) {
     return 0;
 
   pte = walk(pagetable, va, 0);
-  if (pte == 0)
-      return 0;
-  if ((*pte & PTE_V) == 0)
-      return 0;
-  if ((*pte & PTE_U) == 0)
+  if (pte == 0 || (*pte & (PTE_V | PTE_U)) == 0)
     return 0;
+
   pa = PTE2PA(*pte);
-  // 判断写标志位是否没有
+
+  // 判断是否为写时复制的情况
   if ((*pte & PTE_W) == 0) {
-    // pte without COW flag cannot allocate page 
-    if ((*pte & PTE_COW) == 0) {
+    if ((*pte & PTE_COW) == 0)
         return 0;
-    }
-    // 分配新物理页
-    if ((mem = kalloc()) == 0) {
+
+    // 尝试分配新物理页
+    if ((mem = kalloc()) == 0)
       return 0;
-    }
-    // 拷贝页表内容
+
+    // 复制当前页内容
     memmove(mem, (void*)pa, PGSIZE);
-    // 更新标志位
-    flags = (PTE_FLAGS(*pte) & (~PTE_COW)) | PTE_W;
-    // 取消原映射
+
+    // 更新页表项的标志位，替换COW为写标志位
+    flags = (PTE_FLAGS(*pte) & ~PTE_COW) | PTE_W;
+
+    // 解除旧映射并建立新映射
     uvmunmap(pagetable, PGROUNDDOWN(va), 1, 1);
-    // 更新新映射
     if (mappages(pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)mem, flags) != 0) {
       kfree(mem);
       return 0;
     }
-    return (uint64)mem;    // COW情况下返回新物理地址
+
+    return (uint64)mem;  // 返回新物理地址
   }
+
   return pa;
 }
 uint64
